f8b6e1fd5fc7dc7a082df23f2a5b621f2057601e
zliimt717 <xin.gu1707@gmail.com>
2023-04-13 13:01:56 +0200

    qr code for WiFi

--- src/main/java/org/example/MainController.java
+++ src/main/java/org/example/MainController.java
@@ -8,21 +8,26 @@ import org.springframework.web.bind.annotation.GetMapping;
 import java.io.IOException;
 import java.util.Base64;
 
+import static java.lang.String.*;
+
 @Controller
 public class MainController {
 
     private static final String QR_CODE_IMAGE_PATH="src/main/resources/static/img/QRCode.png";
     @GetMapping("/")
     public String getQRCode(Model model){
-        String gitHub="https://github.com/zliimt717";
+        String ssid="test";
+        String password="test";
+        String wifiString="WIFI:T:WPA;S:%s;P:%s;;";
+        wifiString=format(wifiString,ssid,password);
 
         byte[] image=new byte[0];
 
         try {
             // Generate and Return Qr Code in Byte Array
-            image=QRCodeGenerator.getQRCodeImage(gitHub,250,250);
+            image=QRCodeGenerator.getQRCodeImage(wifiString,250,250);
             // Generate and Save Qr Code Image in static/image folder
-            QRCodeGenerator.generateQRCodeImage(gitHub,250,250,QR_CODE_IMAGE_PATH);
+            QRCodeGenerator.generateQRCodeImage(wifiString,250,250,QR_CODE_IMAGE_PATH);
 
         }catch (WriterException | IOException e){
             e.printStackTrace();
@@ -30,7 +35,7 @@ public class MainController {
         // Convert Byte Array into Base64 Encode String
         String qrCode= Base64.getEncoder().encodeToString(image);
 
-        model.addAttribute("github",gitHub);
+        model.addAttribute("wifiString",wifiString);
         model.addAttribute("qrcode",qrCode);
         return "qrcode";
     }
